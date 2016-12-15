c96ee263cdf4737a8966861d5d98262abf5ec62e
Rainy @ aclab laptop <renokx@gmail.com>
2016-12-15 18:27:06 +0800

    modify mqtt part

--- pythonCode/get_data_from_mcs_mqtt.py
+++ pythonCode/get_data_from_mcs_mqtt.py
@@ -11,6 +11,9 @@
 
 import paho.mqtt.client as mqtt
 
+# sys.path.insert(0, '/usr/lib/python2.7/bridge/')
+# from bridgeclient import BridgeClient as bridgeclient
+
 deviceId = "DJRs0jHX"
 deviceKey = "GLBhC0Rrpj6p2KEW"
 MQTT_SERVER = "mqtt.mcs.mediatek.com"
@@ -22,8 +25,25 @@ def on_connect(client, userdata, flags, rc):
     print("MQTT Connected with result code "+str(rc))
     client.subscribe(MQTT_TOPIC)
 
+
 def on_message(client, userdata, msg):
-    print("mqtt payload=%s" %(msg.payload))
+    # print("mqtt payload=%s" %(msg.payload))
+    # mqtt payload=1481790904274,Notification,Hi
+    message = msg.payload.split(",")
+    commadType = message[1]
+    dataInfo = message[2]
+    print message
+    commadSend(commadType, dataInfo)
+
+def commadSend(commadType, dataString):
+	if commadType == "Notification":
+		if dataString == "1":
+			print "1"
+
+		if dataString == "2":
+			print "2"
+
+    # print message[2]
 
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
--- pythonCode/linkit_Smart_7688_MCS.py
+++ pythonCode/linkit_Smart_7688_MCS.py
@@ -32,6 +32,8 @@ deviceKey = "GLBhC0Rrpj6p2KEW"
 dataChnId1 = "Temperature"
 dataChnId2 = "Humidity"
 dataChnId3 = "Dust"
+dataChnId4 = "GPS"
+getChnId1 = "LED"
 MQTT_SERVER = "mqtt.mcs.mediatek.com"
 MQTT_PORT = 1883
 MQTT_ALIVE = 60
@@ -39,11 +41,12 @@ MQTT_TOPIC1 = "mcs/" + deviceId + "/" + deviceKey + "/" + dataChnId1
 MQTT_TOPIC2 = "mcs/" + deviceId + "/" + deviceKey + "/" + dataChnId2
 MQTT_TOPIC3 = "mcs/" + deviceId + "/" + deviceKey + "/" + dataChnId3
 
+MQTT_TOPIC_MONITOR = "mcs/" + deviceId + "/" + deviceKey + "/" + getChnId1
 # *********************************************************************
 # 25.018531, 121.536753
-gps_alt=0
-gps_lat=25.018531
-gps_lon=121.536753
+gps_alt = 0
+gps_lat = 25.018531
+gps_lon = 121.536753
 # *********************************************************************
 
 mqtt_client = mqtt.Client()
@@ -64,10 +67,34 @@ def post_to_mcs(payload):
 
     conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers)
     response = conn.getresponse()
-    print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
+    #print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
+    print response.status
     data = response.read()
     conn.close()
 # *********************************************************************
+# Get data from MQTT Server
+def on_connect(client, userdata, flags, rc):
+    print("MQTT Connected with result code "+str(rc))
+    client.subscribe(MQTT_TOPIC_MONITOR)
+
+def on_message(client, userdata, msg):
+    print("mqtt payload=%s" %(msg.payload))
+# *********************************************************************
+def establishCommandChannel():
+    # Query command server's IP & port
+    connectionAPI = 'https://api.mediatek.com/mcs/v2/devices/%(device_id)s/connections.csv'
+    r = requests.get(connectionAPI % DEVICE_INFO,
+                 headers = {'deviceKey' : DEVICE_INFO['device_key'],
+                            'Content-Type' : 'text/csv'})
+    logging.info("Command Channel IP,port=" + r.text)
+    (ip, port) = r.text.split(',')
+
+    # Connect to command server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, int(port)))
+    s.settimeout(None)
+# ************************************************************************
+# mqtt_client.loop_start()
 
 while True:
     h0 = value.get("h")
@@ -75,20 +102,30 @@ while True:
     d0 = value.get("d")
 
     # value.put("alert", 123)
-
+# ***********************************************************************
+    # MQTT clint publish data
     payload = {"dataChnId":dataChnId1,"value":t0}
-    print dataChnId1 + " : " + t0
+    #print dataChnId1 + " : " + t0
     mqtt_client.publish(MQTT_TOPIC1, json.dumps(payload), qos=0)
 
     payload = {"dataChnId":dataChnId2,"value":h0}
-    print dataChnId2 + " : " + h0
+    #print dataChnId2 + " : " + h0
     mqtt_client.publish(MQTT_TOPIC2, json.dumps(payload), qos=0)
 
     payload = {"dataChnId":dataChnId3,"value":d0}
-    print dataChnId3 + " : " + d0
+    #print dataChnId3 + " : " + d0
     mqtt_client.publish(MQTT_TOPIC3, json.dumps(payload), qos=0)
 
-    payload2 = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}}, {"dataChnId":"Temperature","values":{"value":t0}}, {"dataChnId":"Dust","values":{"value":d0}}, {"dataChnId":"GPS","values":{"latitude":gps_lat, "longitude":gps_lon, "altitude": gps_alt}}]}
+    payload = {"dataChnId":dataChnId4,"value":{"latitude":gps_lat, "longitude":gps_lon, "altitude": gps_alt}}
+    #print dataChnId4 + " : latitude = {LA} | longitude = {LO} | altitude = {AL}".format(LA=gps_lat, LO=gps_lon, AL=gps_alt)
+    mqtt_client.publish(MQTT_TOPIC3, json.dumps(payload), qos=0)
+# ************************************************************************
+    #  Http clint send data
+    payload2 = {"datapoints":[{"dataChnId":dataChnId2,"values":{"value":h0}}, {"dataChnId":dataChnId1,"values":{"value":t0}}, {"dataChnId":dataChnId3,"values":{"value":d0}}, {"dataChnId":dataChnId4,"values":{"latitude":gps_lat, "longitude":gps_lon, "altitude": gps_alt}}]}
     post_to_mcs(payload2)
-    
+# ************************************************************************
+    # mqtt_client.on_connect = on_connect
+    # mqtt_client.on_message = on_message
+    # mqtt_client.connect(MQTT_SERVER, MQTT_PORT, MQTT_ALIVE)
+
     time.sleep(10)
